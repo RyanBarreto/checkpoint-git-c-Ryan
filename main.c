8d43488c9fa1527f1b38b22d9e0a8f978043091f
RyanBarreto <144958582+RyanBarreto@users.noreply.github.com>
2026-04-27 16:23:34 -0300

    Melhora no cabeçalho

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
+    printf("=======================\n");
+    printf("=== Membros do grupo ===\n");
+    printf("=======================\n");
     printf("Ryan Barreto Carlos Dias\n");
     return 0;
 }
